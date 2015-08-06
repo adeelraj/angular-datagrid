8fadf7f59b63280a34de4b4ae881703bd46056f9
OctoD <iamoctod@gmail.com>
2015-08-06 18:22:39 +0200

    some changes

--- octod-datagrid.js
+++ octod-datagrid.js
@@ -82,19 +82,40 @@
       }
     };
 
+    /**
+     * $get main function while using the provider in an angular injected function
+     * @return {Object}
+     */
     this.$get = function () {
       return {
+        /**
+         * returns debug querystring. A simple hack to avoid caching
+         * @return {String}
+         */
         getDebugQuerystring: function () {
           return config.debug ? '?datetime='+ Date.now() : '';
         },
+        /**
+         * the partials path. Useful to set where the directive should pick the view
+         * @type {String}
+         */
         partialsPath: config.partials.path
       };
     }
 
+    /**
+     * sets debugmode on/off
+     * @param  {Boolean} debugmode
+     * @return {Undefined}
+     */
     this.debug = function (debugmode) {
       config.debug = debugmode;
     }
 
+    /**
+     * sets the partialsPath parameter used by $get.partialsPath
+     * @param {String} folder 
+     */
     this.setPartialsFolder = function (folder) {
       if (typeof folder === 'string') config.partials.path = folder;
     }
@@ -411,95 +432,9 @@
   .service('OctodLimiter', function () {
     /**
      * OctodLimiter constructor
-     * @param {Number} current      the current index
-     * @param {Number} count        the number of elements available for pagination
-     * @param {Number} limit        the number of visible elements to display
      */
-    function OctodLimiter (current, count, limit) {
-      this.count = count;
-      this.current = current;
-      this.limit = limit;
-      this.beforeAfter = Math.floor(this.limit / (limit % 2 == 0 ? 2 : 3));
+    function OctodLimiter () {
     };
-
-    /**
-     * adds items after
-     * used when items before have a negative index
-     * @private
-     * @param {Array} itemsSet the set of items
-     * @param {Number} expectedLength    the expectedLength
-     */
-    function addItemsAfter (itemsSet, expectedLength) {
-      var array = [].concat(itemsSet);
-      for (var i = itemsSet.length; i <= expectedLength; i++) array.push(i);
-      return array;
-    }
-
-    /**
-     * adds items after
-     * used when items after are greater than the counted items
-     * @private
-     * @param {Array} itemsSet the set of items
-     * @param {Number} expectedLength    the expectedLength
-     */
-    function addItemsBefore (itemsSet, expectedLength) {
-      var array = [].concat(itemsSet);
-      for (var i = itemsSet.length; i <= expectedLength; i--)
-        if (i > 0) array.push(i);
-      return array;
-    }
-
-    /**
-     * filters negative items
-     * @private
-     * @param  {Array} itemsSet
-     * @return {Array}
-     */
-    function filterNegatives (itemsSet) {
-      return itemsSet.filter(function (item) {
-        return item >= 0;
-      });
-    }
-
-    /**
-     * gets items before the current index
-     * @return {Array}
-     */
-    OctodLimiter.prototype.getItemsAfter = function () {
-      var items = [];
-      for (var i = this.current; i < this.current + this.beforeAfter; i++) items.push(i);
-      return items;
-    }
-
-    /**
-     * gets items after the current index
-     * @return {Array}
-     */
-    OctodLimiter.prototype.getItemsBefore = function () {
-      var items = [];
-      for (var i = this.current; i > this.current - this.beforeAfter; i--) items.push(i);
-      return items;
-    }
-
-    /**
-     * returns the range of items selectable
-     * @return {Array} the range
-     */
-    OctodLimiter.prototype.getRange = function () {
-      var range = [];
-      return range;
-    }
-
-    /**
-     * returns if the provided number is in the current yelded range
-     * @param  {Number} number the number
-     * @return {Boolean}
-     */
-    OctodLimiter.prototype.inRange = function (number) {
-      return number in this.getRange();
-    }
-
-    //console.log(addItemsBefore([8], 12));
 
     // returning constructor
     return OctodLimiter;
@@ -613,9 +548,6 @@
       this.redraw();
       this.pagination = new OctodPagination(this.rows, this.config.pagination);
       this.pagination.getFirstPage();
-      this.limiter = new OctodPagination(this.pagination.getPages(), this.config.paginator);
-      this.limiter.getPages();
-      this.limiter.getFirstPage();
       return this;
     };
 
